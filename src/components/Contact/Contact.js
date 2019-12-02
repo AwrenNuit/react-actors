38c1c0678efd7f65894d4b0f478d68b5fa5a7a1b
AwrenNuit <a.rennaker@yahoo.com>
2019-12-02 17:36:23 -0600

    completed some stretch goals

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,15 +1,55 @@
 import React from 'react';
 
 class Contact extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            fname: '',
+            lname: '',
+            impression: ''
+        };
+    
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onButtonClick = this.onButtonClick.bind(this);
+      }  
+        
+    onInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({[name]: value});
+    }
+
+    onButtonClick(event){
+        alert(`Thank you for joining, ${this.state.fname}!`);
+        event.preventDefault();
+
+        this.setState({
+            fname: '',
+            lname: '',
+            impression: ''
+        })
+    }
+
     render(){
         return(
             <>
-                <h2 id="contact">Contact</h2>
-                <p id="phone">1-800-REACT</p>
-                <p id="email">reactactors@actorsreact.com</p>
+                <form onSubmit={this.onButtonClick}>
+                    <h2 id="contact">Contact</h2>
+                    <input onChange={this.onInputChange} className="contact-input" type="text" name="fname" value={this.state.fname} placeholder="First Name" />
+                    <input onChange={this.onInputChange} className="contact-input" type="text" name="lname" value={this.state.lname} placeholder="Last Name" />
+                    <input onChange={this.onInputChange} className="contact-input" type="text" name="impression" value={this.state.impression} placeholder="Best Impression" />
+                    <input type="submit" value="Submit" id="submit-btn" />
+                    {/* unsure how to make input values append to p tag only on last input text */}
+                    <p>{`${this.state.fname} ${this.state.lname} ${this.state.impression}`}</p>
+                    <p id="phone">Phone: 1-800-WE-REACT</p>
+                    <p id="email">email: info@reactactors.org</p>
+                </form>
             </>
         )
     }
 }
+
+
 
 export default Contact;
