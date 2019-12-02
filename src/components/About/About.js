fdc4b8b4b53cbdd1f012988b1d1c753ec813f35f
AwrenNuit <a.rennaker@yahoo.com>
2019-12-02 15:52:51 -0600

    basic layout achieved

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component{
+    render(){
+        return(
+            <>
+                <h2 id="about">About</h2>
+                <p id="about-us">We are actors that react to people reacting to actors.</p>
+            </>
+        )
+    }
+}
+
+export default About;
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Contact extends React.Component{
+    render(){
+        return(
+            <>
+                <h2 id="contact">Contact</h2>
+                <p id="phone">1-800-REACT</p>
+                <p id="email">reactactors@actorsreact.com</p>
+            </>
+        )
+    }
+}
+
+export default Contact;
